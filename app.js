aa9a4ab5acf93c33a126cecc3c5d35f304cb2314
john-seen <73298797+john-seen@users.noreply.github.com>
2024-11-22 12:57:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+mongoose.connect('mongo://', {
+    user: super11111111111,
+    pass: Super123,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
